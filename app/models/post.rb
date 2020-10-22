54e33578a04e455bddb97887659714c1dd8bc178
patrick-rush <patrick.a.rush@gmail.com>
2020-10-21 20:40:00 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,4 +17,35 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+
+  def self.by_author(author_id)
+    where(author: author_id)
+  end
+
+  def index
+    # provide a list of authors to the view for the filter control
+    @authors = Author.all
+   
+    # filter the @posts list based on user input
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
+    else
+      # if no filters are applied, show all posts
+      @posts = Post.all
+    end
+  end
+
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+    
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
 end
